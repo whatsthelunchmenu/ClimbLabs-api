0358fcdc7ead76a9d448174381e023954ad08613
phantom08266 <phantom08266@gmail.com>
2021-11-04 16:40:50 +0900

    chore : 프로젝트 초기 셋팅

---
+++ src/main/java/com/example/climblabs/ClimbLabsApplication.java
@@ -0,0 +1,13 @@
+package com.example.climblabs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClimbLabsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClimbLabsApplication.class, args);
+    }
+
+}
