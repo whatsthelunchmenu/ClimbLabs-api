767aaec84753005a1722f9177aec11e73635c292
phantom08266 <phantom08266@gmail.com>
2021-12-13 23:22:53 +0900

    feat(#12): 환경에 따른 image 저장방식 달리 적용(@Profile 사용)

--- src/main/java/com/example/climblabs/global/utils/image/LocalSaveFileUtils.java
+++ src/main/java/com/example/climblabs/global/utils/image/LocalSaveFileUtils.java
@@ -1,29 +1,23 @@
 package com.example.climblabs.global.utils.image;
 
+import com.example.climblabs.global.exception.OtherPlatformHttpException;
+import com.example.climblabs.global.utils.image.dto.ImageFileDto;
 import java.io.File;
 import java.io.FileOutputStream;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 import java.util.function.Function;
 import java.util.stream.Collectors;
-
-import com.example.climblabs.global.exception.OtherPlatformHttpException;
-import com.example.climblabs.global.utils.image.dto.ImageFileDto;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.logging.log4j.util.Strings;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
-import org.springframework.util.CollectionUtils;
 import org.springframework.web.multipart.MultipartFile;
 
-@Profile(value = {"local", "dev"})
+@Profile(value = "default")
 @Slf4j
 @Component
 public class LocalSaveFileUtils implements ImageStorageUtils {
-
 
     @Override
     public List<ImageFileDto> saveToStorage(List<MultipartFile> images) {
