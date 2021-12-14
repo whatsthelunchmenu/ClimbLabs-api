94931213d1685cef6b85d768a06cb734fac59153
phantom08266 <phantom08266@gmail.com>
2021-12-14 12:02:52 +0900

    feat(#19) : 게시물 칼럼추가된 부분 ddl 변경 및 h2 db를 mysql로 설정변경

--- sql/ddl.sql
+++ sql/ddl.sql
@@ -33,20 +33,41 @@ create table member
     primary key (member_id)
 ) engine=InnoDB;
 
-create table post
-(
-    post_id        bigint  not null auto_increment,
-    city           varchar(255),
-    detail_street  varchar(255),
-    street         varchar(255),
-    zip_code       varchar(255),
+create table post (
+    post_id bigint not null auto_increment,
+    city varchar(255),
+    detail_street varchar(255),
+    street varchar(255),
+    zip_code varchar(255),
     climbing_title varchar(255),
-    created_at     datetime(6),
-    feature        varchar(255),
-    level          integer not null,
-    scale_type     varchar(255),
-    title          varchar(255),
-    updated_at     datetime(6),
-    member_id      bigint,
+    created_at datetime,
+    feature varchar(255),
+    level integer not null,
+    scale integer,
+    scale_type varchar(255),
+    title varchar(255),
+    updated_at datetime,
+    member_id bigint,
     primary key (post_id)
-) engine=InnoDB;
+) engine=InnoDB
+
+
+alter table advantage
+    add constraint FKc7a2rfe5m10aks8qdqj3qc7ej
+        foreign key (post_id)
+            references post (post_id);
+
+alter table dis_advantage
+    add constraint FKqo6ym272lph8q6yymsj4kljqd
+        foreign key (post_id)
+            references post (post_id);
+
+alter table image
+    add constraint FKe2l07hc93u2bbjnl80meu3rn4
+        foreign key (post_id)
+            references post (post_id);
+
+alter table post
+    add constraint FK83s99f4kx8oiqm3ro0sasmpww
+        foreign key (member_id)
+            references member (member_id);
