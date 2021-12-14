9761f23815abde01a96d79dab5ed7ab12604fe1a
phantom08266 <phantom08266@gmail.com>
2021-12-14 14:33:09 +0900

    feat(#19) : DataJpaTest 애노테이션이 설정한 DB에서 동작하도록 설정

--- src/test/java/com/example/climblabs/member/domain/MemberTest.java
+++ src/test/java/com/example/climblabs/member/domain/MemberTest.java
@@ -5,12 +5,14 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 
 import java.time.LocalDateTime;
 
 @DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 class MemberTest {
 
     @Autowired
