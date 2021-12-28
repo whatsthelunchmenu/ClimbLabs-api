0ef5e06032fb72da5981dab57422102bb0d3defc
phantom08266 <phantom08266@gmail.com>
2021-12-28 18:42:07 +0900

    feat(#33): 이미지 크기 벗어날 시 예외처리

--- src/main/java/com/example/climblabs/global/exception/ExceptionCode.java
+++ src/main/java/com/example/climblabs/global/exception/ExceptionCode.java
@@ -7,10 +7,19 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public enum ExceptionCode {
 
-    //file
+    /*
+        파일관련 에러
+     */
+    //파일 저장 실패 시
     FAIL_SAVE_IMAGE(500, "이미지 파일을 저장 실패했습니다."),
+    //요청 용량 초과 시
+    SizeLimitExceededException(500, "요청 용량 초과되었습니다."),
+    //파일 용량 초과 시
+    FileSizeLimitExceededException(500, "요청한 이미지 중 용량초과된 이미지가 포함되어있습니다."),
 
-    //platform
+    /*
+        외부 플랫폼 연동에러
+     */
     OTHER_PLATFORM_HTTP_ERROR(500, "외부 플랫폼에서 에러가 발생했습니다. 다시 시도해주세요.");
 
     private final int status;
