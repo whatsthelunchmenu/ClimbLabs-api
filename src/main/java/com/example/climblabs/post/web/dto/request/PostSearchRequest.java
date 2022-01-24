5b595b61e9c739cd62b2c16f5f9d0960cc0ab539
phantom08266 <phantom08266@gmail.com>
2022-01-24 11:18:57 +0900

    feat(#49): 코드 리팩토링
    관리자 페이지에서 사용했던 코드 제거

--- src/main/java/com/example/climblabs/post/web/dto/request/PostSearchRequest.java
+++
@@ -1,9 +0,0 @@
-package com.example.climblabs.post.web.dto.request;
-
-import lombok.Value;
-
-@Value
-public class PostSearchRequest {
-    private String searchType;
-    private String searchValue;
-}
