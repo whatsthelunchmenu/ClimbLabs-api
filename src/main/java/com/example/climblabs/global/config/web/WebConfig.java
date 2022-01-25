6ca8e50278997021946e173b29659c1461ec97c5
phantom08266 <phantom08266@gmail.com>
2022-01-25 16:03:02 +0900

    feat(#52): CORS 세부설정 적용
    - maxAge시간만큼 pre-flight 요청 시 캐싱 해둘 수 있도록 설정
    - GET, POST 메서드만 허용하도록 메소드 지정

--- src/main/java/com/example/climblabs/global/config/web/WebConfig.java
+++ src/main/java/com/example/climblabs/global/config/web/WebConfig.java
@@ -9,7 +9,9 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/search/posts")
-            .allowedOrigins("*");
+        registry.addMapping("/search/*")
+            .allowedOrigins("*")
+            .allowedMethods("GET", "POST")
+            .maxAge(3000);
     }
 }
