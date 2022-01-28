1e51c616147c2ff160437f7a030eeb43e4b6bc53
Lee June Hee <39672033+phantom08266@users.noreply.github.com>
2022-01-28 16:28:01 +0900

    feat(#52): 관리자 페이지 CORS 이슈처리 (#53)
    * feat(#52): CORS 설정 적용
    
    * feat(#52): CORS 세부설정 적용
    
    - maxAge시간만큼 pre-flight 요청 시 캐싱 해둘 수 있도록 설정
    - GET, POST 메서드만 허용하도록 메소드 지정
    
    * feat(#52): 관리자 페이지 - 등록, 수정 시 CORS 적용
    
    * feat(#52): 관리자 페이지 - CORS 업데이트 시 patch 메서드도 허용

---
+++ src/main/java/com/example/climblabs/global/config/web/WebConfig.java
@@ -0,0 +1,22 @@
+package com.example.climblabs.global.config.web;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/search/*")
+            .allowedOrigins("*")
+            .allowedMethods("GET", "POST")
+            .maxAge(3000);
+
+        registry.addMapping("/posts/*")
+            .allowedOrigins("*")
+            .allowedMethods("GET", "POST", "PATCH")
+            .maxAge(3000);
+    }
+}
